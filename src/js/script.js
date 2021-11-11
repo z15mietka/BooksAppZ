3fd3d550f2b918c16d90d9e4ad15d88e48a10d7e
Zaneta Mietka <z.mietka@gmail.com>
2021-11-11 10:39:42 +0100

    Add new features

--- src/js/script.js
+++ src/js/script.js
@@ -66,4 +66,87 @@ class BookList {
         }
       });
   }
-}}
+  /* FILTERS */
+
+  console.log('filters', this.filters);
+  const form = document.querySelector('.filters');
+
+  form.addEventListener('click', function (event) {
+    const clickedElement = event.target;
+
+    const tagName = clickedElement.tagName;
+
+    const type = clickedElement.getAttribute('type');
+
+    const name = clickedElement.getAttribute('name');
+
+    const value = clickedElement.getAttribute('value');
+
+    const clickedElementIsCheckbox =
+      tagName == 'INPUT' && type == 'checkbox' && name == 'filter';
+
+    if (clickedElementIsCheckbox) {
+      if (clickedElement.checked) {
+        console.log('Add', value, 'to filters');
+        thisBookList.filters.push(value);
+      } else {
+        console.log('Remove', value, 'from filters');
+        const valueIndex = thisBookList.filters.indexOf(value);
+        thisBookList.filters.splice(valueIndex, 1);
+      }
+    }
+    thisBookList.filterBooks();
+  });
+}
+
+/* FILTER BOOKS */
+
+filterBooks() {
+  const thisBookList = this;
+
+  for (let eachBookdata of dataSource.books) {
+    let shouldBeHidden = false;
+
+    for (const option of this.filters) {
+      if (!eachBookdata.details[option] == false) {
+        shouldBeHidden = true;
+        break;
+      }
+    }
+
+    const bookImage = thisBookList.booksList.querySelector(
+      // '.book__image[data-id="' + eachBookdata.id + '"'
+      `.book__image[data-id="${eachBookdata.id}"]`
+    );
+
+    if (shouldBeHidden) {
+      bookImage.classList.add('hidden');
+    } else {
+      bookImage.classList.remove('hidden');
+    }
+  }
+}
+
+/* Ratings */
+
+determineRatingBgc(rating) {
+  let ratingBackground = '';
+
+  if (rating < 6) {
+    ratingBackground =
+      'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+  } else if (rating > 6 && rating <= 8) {
+    ratingBackground = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%);';
+  } else if (rating > 8 && rating <= 9) {
+    ratingBackground = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+  } else if (rating > 9) {
+    ratingBackground = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+  }
+  return ratingBackground;
+
+  // console.log("ratingBackground", ratingBackground);
+}
+}
+
+// eslint-disable-next-line no-unused-vars
+const app = new BookList();
